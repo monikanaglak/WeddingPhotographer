2b4054c779c75a1a18e90a6c3525774ae79742c0
monika <monikanaglak@outlook.fr>
2024-02-13 16:36:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+/*
+let hero = document.querySelector(".hero");
+let herotwo = document.querySelector(".hero-two");
+let heroone = document.querySelector(".hero-one");
+let slider = document.querySelector(".slider");
+let responsive_menu = document.querySelector(".responsive-menu");
+
+const t1 = new TimelineMax();
+t1.fromTo(hero,3.5,{height:"0%"},{height:"100%"})
+
+.fromTo(slider,1.5,{x:"-100%"},{x:"0%"})
+
+
+let hambourger = document.querySelector(".hamburger");
+hambourger.addEventListener("click",()=>{
+    console.log("click")
+    responsive_menu.classList.add("visible")
+})
+*/
